17d9f751e962675f4493ce12c18d52d3dd520ab0
Ogbonna_Amuche <amuche247cynthia@gmail.com>
2021-07-22 13:51:39 +0100

    added .gitignore file

--- filesharing/settings.py
+++ filesharing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%6d&80nljs49ztb*5+l-0bh5_)+3!-cz=8zv+y-u#)n9eb-f59'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
